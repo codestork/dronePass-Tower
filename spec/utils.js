81efe3eb8aa96558c2720f58d557d45d99c41f4f
davidraleigh <davidraleigh@gmail.com>
2015-01-07 17:23:10 -0800

    clean up code and add test for getPathConflicts

--- spec/utils.js
+++ spec/utils.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 var pg = require(path.join(__dirname, '..', './server/db/config.js'));
 var utils = require(path.join(__dirname, '..', './server/db/utils.js'));
 
-var TIME_OUT = 250;
+var TIME_OUT = 1000;
 
 describe('addFlightPath()', function () {
   'use strict';
@@ -15,22 +15,22 @@ describe('addFlightPath()', function () {
   it('should fail to add a flight path with incorrect time order', function(done) {
     var linestring = {"type":"LineString","coordinates":[[-121.78344913643356,37.523210405243056],[-121.78061820472406,37.51003985009118],[-121.76222428103338,37.51429365145312],[-121.76742368405428,37.50678829541209]]};
 
-    // requires a drone to be in database
-    var drone_id = 666;
-    // requires a drone_operator to be in database
-    var drone_operator_id = 666;
-    // requires 3 land owners to be in database
-    // landOwnerIds : 666, 667, 668
-    // parcel ids : 328449, 328451, 328452
-    // requires a restricted parcel 328449
-    // requires a restricted parcel with an exemption 328451
-    // requires a parcel that doesn't have restrictions 328452
+    /** requires a drone to be in database
+     requires a drone_operator to be in database
+     requires 3 land owners to be in database
+     landOwnerIds : 666, 667, 668
+     parcel ids : 328449, 328451, 328452
+     requires a restricted parcel 328449
+     requires a restricted parcel with an exemption 328451
+     requires a parcel that doesn't have restrictions 328452
     
-    //registerAddress(666, 328449, '04:05:06', '10:05:06');
-    //registerAddress(667, 328451, null, null);
-    //registerAddress(668, 328452, '04:05:06', '10:05:06');
+     registerAddress(666, 328449, '04:05:06', '10:05:06');
+     registerAddress(667, 328451, null, null);
+     registerAddress(668, 328452, '04:05:06', '10:05:06');
+    **/
+    var drone_id = 666;
+    var drone_operator_id = 666;
     var errResult;
-    var flightPathId;
     utils.addFlightPath(drone_id, drone_operator_id, "1999-01-08 05:05:06", "1999-01-08 04:05:06", "'" + JSON.stringify(linestring) + "'").exec(function(err, r) {
       errResult = err.routine;
     });
@@ -44,20 +44,23 @@ describe('addFlightPath()', function () {
   it('should add flight path', function(done) {
     var result;
     var linestring = {"type":"LineString","coordinates":[[-121.78344913643356,37.523210405243056],[-121.78061820472406,37.51003985009118],[-121.76222428103338,37.51429365145312],[-121.76742368405428,37.50678829541209]]};
-    // requires a drone to be in database
-    var drone_id = 666;
-    // requires a drone_operator to be in database
-    var drone_operator_id = 666;
-    // requires 3 land owners to be in database
-    // landOwnerIds : 666, 667, 668
-    // parcel ids : 328449, 328451, 328452
-    // requires a restricted parcel 328449
-    // requires a restricted parcel with an exemption 328451
-    // requires a parcel that doesn't have restrictions 328452
+    /** requires a drone to be in database
+     requires a drone_operator to be in database
+     requires 3 land owners to be in database
+     landOwnerIds : 666, 667, 668
+     parcel ids : 328449, 328451, 328452
+     requires a restricted parcel 328449
+     requires a restricted parcel with an exemption 328451
+     requires a parcel that doesn't have restrictions 328452
     
-    //registerAddress(666, 328449, '04:05:06', '10:05:06');
-    //registerAddress(667, 328451, null, null);
-    //registerAddress(668, 328452, '04:05:06', '10:05:06');
+     registerAddress(666, 328449, '04:05:06', '10:05:06');
+     registerAddress(667, 328451, null, null);
+     registerAddress(668, 328452, '04:05:06', '10:05:06');
+    **/
+
+    
+    var drone_id = 666;
+    var drone_operator_id = 666;
     var flightPathId;
     utils.addFlightPath(drone_id, drone_operator_id, "1999-01-08 04:05:06", "1999-01-08 15:05:06", "'" + JSON.stringify(linestring) + "'").exec(function(err, r) {
 
@@ -88,4 +91,23 @@ describe('getPathConflicts()', function() {
   it('exists getPathConflicts', function () {
     expect(utils.getPathConflicts).to.be.a('function');
   });
+
+  it('should return restricted geometries from getPathConflicts', function (done) {
+    var linestring = {"type":"LineString","coordinates":[[-121.78344913643356,37.523210405243056],[-121.78061820472406,37.51003985009118],[-121.76222428103338,37.51429365145312],[-121.76742368405428,37.50678829541209]]};
+
+    var drone_id = 666;
+    var drone_operator_id = 666;
+    var result;
+    utils.getPathConflicts(drone_id, drone_operator_id, "1999-01-08 04:05:06", "1999-01-08 10:05:06", "'" + JSON.stringify(linestring) + "'").exec(function(err, r) {
+      
+      result = r;
+    });
+    
+    setTimeout(function() {
+      console.log('argh');
+      console.log(result);
+      expect(1).to.equal(2);
+      done();
+    }, TIME_OUT);    
+  });  
 })
